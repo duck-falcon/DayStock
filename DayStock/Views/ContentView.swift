1f67f789372f6f619a768b7e03c0324e1396fec7
Hikaru Midorikawa <midoriiro1803@gmail.com>
2025-08-23 19:39:31 +0900

    first release

--- DayStock/Views/ContentView.swift
+++ DayStock/Views/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     }
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ZStack {
                 if store.appState.items.isEmpty {
                     emptyStateView
